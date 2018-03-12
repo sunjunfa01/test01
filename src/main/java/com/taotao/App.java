9d935a60a69d782ebcc889fe739d429a557f0adb
sunjunfa01 <403366530@qq.com>
2018-03-12 09:33:38 +0800

    第一次测试

---
+++ src/main/java/com/taotao/App.java
@@ -0,0 +1,15 @@
+package com.taotao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
